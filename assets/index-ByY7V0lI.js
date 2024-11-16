ecb32ed20b211482d58ab774594b9a4d1cae1902
363045841 <161981174@qq.com>
2024-11-16 18:07:14 +0800

    更新base

--- assets/index-ByY7V0lI.js
+++
@@ -1,1 +0,0 @@
-import{d as e,r as n,o as r,c as t}from"./index-DOJyo8qS.js";const _=e({__name:"index",setup(c){return(s,a)=>{const o=n("HelloWorld");return r(),t(o)}}});export{_ as default};
---
+++ assets/index-D5Ix69qX.js
@@ -0,0 +1,1 @@
+import{d as e,o as n,c as r,r as t}from"./index-DAdcCYlv.js";const _=e({__name:"index",setup(c){return(s,a)=>{const o=t("HelloWorld");return n(),r(o)}}});export{_ as default};
