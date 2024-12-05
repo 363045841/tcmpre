2babde2cb1b9f295004758ecf4c6402d7c8afd8f
363045841 <161981174@qq.com>
2024-12-05 17:16:41 +0800

    添加历史记录占位，搜索结果闪烁修复

---
+++ assets/default-yMSuKLL2.js
@@ -0,0 +1,1 @@
+import{d as r,o as p,c as s,w as o,a as e,r as t,V as _,b as c}from"./index-Vjgxr_fm.js";const f=r({__name:"default",setup(l){return(u,m)=>{const a=t("router-view"),n=t("AppFooter");return p(),s(c,null,{default:o(()=>[e(_,null,{default:o(()=>[e(a)]),_:1}),e(n)]),_:1})}}});export{f as default};
---
+++ assets/test-C_x4yDOQ.js
@@ -0,0 +1,1 @@
+import{d as e,o as t,aj as o}from"./index-Vjgxr_fm.js";const c=e({__name:"test",setup(n){return(a,r)=>(t(),o("h1",null,"Hello"))}});export{c as default};
